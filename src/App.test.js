084c7eef710b9c8983de5fc0507ab9867e028813
jusbuckingham <jus.buckingham@gmail.com>
2021-12-18 15:53:55 -0800

    added a test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Payments', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Payments/i);
   expect(linkElement).toBeInTheDocument();
 });
