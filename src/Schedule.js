ac417b1df9c4e57ca8f4dc9cecd5a20539f71de1
jusbuckingham <jus.buckingham@gmail.com>
2021-12-18 17:00:58 -0800

    building out components

---
+++ src/Schedule.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class Schedule extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+
+        return (
+
+
+            
+
+        );
+
+    }
+}
+
+
+export default Schedule;
---
+++ src/Scores.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class Scores extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+
+        return (
+
+
+          
+
+        );
+
+    }
+}
+
+
+export default Scores;
